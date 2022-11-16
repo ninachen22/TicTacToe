964ab251a53439a1ebec0e8cca4f25bb56cebf2a
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-15 19:35:51 -0500

    passed all tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,81 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
+    //if x wins
     public Boolean isInFavorOfX() {
-        return null;
+        if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X') { //1st horizontal
+            return true;
+        } else if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') { //2nd horizontal
+            return true;
+        } else if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X') { //3rd horiz
+            return true;
+        } else if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X') { //1st vert
+            return true;
+        } else if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X') { //2nd vert
+            return true;
+        } else if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X') { //3rd vert
+            return true;
+        } else if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') { //diag \
+            return true;
+        } else if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X') { //diag/
+            return true;
+        } else {
+            return false;
+        }
+//        return null;
     }
 
+    //if o wins
     public Boolean isInFavorOfO() {
-        return null;
+        if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O') { //1st horizontal
+            return true;
+        } else if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O') { //2nd horizontal
+            return true;
+        } else if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O') { //3rd horiz
+            return true;
+        } else if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O') { //1st vert
+            return true;
+        } else if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O') { //2nd vert
+            return true;
+        } else if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O') { //3rd vert
+            return true;
+        } else if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') { //diag \
+            return true;
+        } else if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O') { //diag/
+            return true;
+        } else {
+            return false;
+        }
+//        return null;
     }
 
+    //if no ones wins
     public Boolean isTie() {
-        return null;
+        if (!isInFavorOfX() && !isInFavorOfO()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    //who won
     public String getWinner() {
-        return null;
+        String winnerIs = "";
+        if (isInFavorOfX()) {
+            winnerIs = "X";
+        } else if (isInFavorOfO()) {
+            winnerIs = "O";
+        } else {
+            winnerIs = "";
+        }
+        return winnerIs;
+//        return null;
     }
 
 }
